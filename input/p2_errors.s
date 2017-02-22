de5268be8f10c0ccad191b789bd0ff8ce6995148
Karsten Fonseca <kfonseca@berkeley.edu>
2017-02-21 21:14:01 -0800

    proj2 submission 2-1 submission 2

--- input/p2_errors.s
+++ input/p2_errors.s
@@ -1,9 +1,93 @@
-		addiu $t0, $t3, $t3				# not a number
-label:	jal								# no label
-		ori $t2, $99, 0xAB				# invalid register
-
-# Things to ignore
-		bne $t0, $t1, not_found			# nonexistant label
-		addiu $t3 $t5 0x80808080		# number too large
-
-# Can you think of any others?
+ELF
+@
+¸
+¸
+¸
+¸
+
+¸
+¸
+¸
+¸
+¸
+¸
+¸
+¸
+¸
+¸
+¸
+¸
+¸
+¸
+¸
+¸
+¸
+¸
+¸
+¸
+¸
+¸
+¸
+¸
+H‰Æ¿
+¸ÿÿÿÿéŒ
+¸ÿÿÿÿéœ
+¸ÿÿÿÿé—
+¸ÿÿÿÿé•
+¸ÿÿÿÿé•
+¸ÿÿÿÿé—
+¸ÿÿÿÿé›
+.symbol
+.relocation
+Œƒ 
+@
+@
+@
+@
+@
+@
+@
+@
+@
+e
+Ë
+‘ 
+ğ@
+
+
+½
+B
+•
+•
+e
+Ë
+œ
+s
+Ë
+
+?
+e
+Ë
+
+-
+
+
+I
+
+
+
+.åu .Z=Y1»Kuv.ZóY!ó‘vwwZ .0/åÉ
+t5hu
+h
+f»w‘Y0„u¡!Y$
+nused2
+lt_div
+@
+w
+@
+@
+@
+
+@
+it_array_entry
+_for_symbol
